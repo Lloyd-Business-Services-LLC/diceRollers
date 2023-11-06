dea5a021a8558dce97780c34afac4d6e55fcc82b
glloydNAT <lloyd.business.holdings@gmail.com>
2023-11-05 18:51:19 -0700

    Update app.js

--- nodejs/app.js
+++ nodejs/app.js
@@ -3,7 +3,7 @@ const Legate = require('./libs/jslegate/index.js');
 const Router = require('./public/router.js');
 var PORT = 8001;
 var portReq = process.argv[2];
-if (!Number.isNaN(portReq) && portReq > 1024 && portReq < 65535){
+if (!Number.isNaN(portReq) && portReq >= 1024 && portReq <= 65535){
   PORT = process.argv[2];
 }
 var hts = new Legate.httpServerV1;
