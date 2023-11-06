2ab57007fc54cb4ccb2ac46d68abaadedb77c9a1
glloydNAT <lloyd.business.holdings@gmail.com>
2023-11-05 17:07:51 -0700

    Update server.js

--- nodejs/libs/jslegate/protocols/http/v1/server.js
+++ nodejs/libs/jslegate/protocols/http/v1/server.js
@@ -2,26 +2,19 @@ var http = require('http'),
     url = require('url'),
     server;
 
-
 module.exports = class HttpServerV1 {
-  constructor(){
-
-  }
+  constructor(){}
 
   start(portNum, router){
-
       function createServer(portNum, router, req, res){
-            server = http.createServer(function (req, res) {  //CONTROLLER
-
+            server = http.createServer(function (req, res) {
             // client path request
             let path = url.parse(req.url, req.port=portNum).pathname;
           
-            //see sse response for use with req.on
-            router.loadPage(res,path, req);
+            router.loadPage(res,path,req);
           
             }).listen(portNum);
             console.log("http server is listening on port: " + portNum);
-
 
         return server;
       }
